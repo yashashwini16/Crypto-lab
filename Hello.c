0cfe01d865b9868a4f19e1a8ea7c9d61a6844505
yashashwini16 <yashaswinib1603@gmail.com>
2023-04-16 14:31:59 +0530

    1

--- Hello.c
+++ Hello.c
@@ -4,4 +4,10 @@ void main()
 {
     char str[],str1[];
     str[]="Hello world";
+    l=len(str)
+    for(int i=0;i<=l;i++){
+        str1=str^0;
+        printf("%c",str1[i]);
+    }
+    printf("\n");
 }
